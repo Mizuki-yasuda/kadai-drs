bb3149a5c7c3dfe89e12ae34dcb4f81c5f7530a8
mizuki.yasuda <mizuki.y71@gmail.com>
2020-11-04 23:38:11 +0900

    Add Costomer index

---
+++ src/controllers/customers/CustomersUpdateServlet.java
@@ -0,0 +1,32 @@
+package controllers.customers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CustomersUpdateServlet
+ */
+@WebServlet("/customers/update")
+public class CustomersUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CustomersUpdateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
