86f624fdb3bf01aad9b16dc853fa19b99a676293
mizuki.yasuda <mizuki.y71@gmail.com>
2020-11-07 14:15:20 +0900

    Add Customer edit

--- src/controllers/customers/CustomersEditServlet.java
+++ src/controllers/customers/CustomersEditServlet.java
@@ -1,19 +1,26 @@
 package controllers.customers;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Customer;
+import models.Employee;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class CustomersEditServlet
  */
 @WebServlet("/customers/edit")
 public class CustomersEditServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +29,25 @@ public class CustomersEditServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Customer c = em.find(Customer.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        Employee login_employee = (Employee)request.getSession().getAttribute("login_employee");
+        if(c != null && login_employee.getId() == c.getEmployee().getId()) {
+            request.setAttribute("customer", c);
+            request.setAttribute("_token", request.getSession().getId());
+            request.getSession().setAttribute("customer_id", c.getId());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/customers/edit.jsp");
+        rd.forward(request, response);
+    }
 
 }
